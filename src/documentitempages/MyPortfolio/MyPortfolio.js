47fed92f2e5400f4bce6f85c6271910697ce0998
Miguel Paulo Capili <mpgcapili@gmail.com>
2022-06-09 16:01:15 +0800

    update the Demo source

--- src/documentitempages/MyPortfolio/MyPortfolio.js
+++ src/documentitempages/MyPortfolio/MyPortfolio.js
@@ -387,7 +387,7 @@ const MyPortfolio = () => {
                             // width="853"
                             // height="480"
 
-                            src="https://www.youtube.com/embed/uGRtRyztdbM"
+                            src="https://www.youtube.com/embed/6Nxuek8FhuI"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
